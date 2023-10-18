14c411e6d1892e430afce0ec17ba8a3ff3585949
HaydnRoberts <hr342208@truro-penwith.ac.uk>
2023-10-18 15:24:02 +0100

    init

---
+++ javascript/sign_in.js
@@ -0,0 +1,30 @@
+// Add an event listener to the form submission
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector("form");
+    const submitButton = form.querySelector("button[type=submit]");
+
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        // Get the user's email input
+        const email = document.getElementById("email").value;
+
+        // You can add more fields and validation here if needed
+
+        // Lock out the submit button
+        submitButton.disabled = true;
+        submitButton.textContent = "Thank you for signing in"
+
+        // Simulate form processing (you should replace this with your actual server-side processing)
+        setTimeout(function () {
+            // Reset the form
+            form.reset();
+
+            // Display a success message
+            const confirmationMessage = document.createElement("p");
+            confirmationMessage.innerText = `Hello, ${email}! You are now signed in.`;
+            confirmationMessage.classList.add("text-green-500", "my-4", "text-xl");
+            form.parentNode.appendChild(confirmationMessage);
+        }, 500); // Simulating a 0.5-second delay (you can remove this in your actual implementation)
+    });
+});
