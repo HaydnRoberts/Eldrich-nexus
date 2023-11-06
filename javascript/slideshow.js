d15f8e393104b3595df1d830bb90cc0238b4ed6a
HaydnRoberts <hr342208@truro-penwith.ac.uk>
2023-11-06 13:39:09 +0000

    final update

--- javascript/slideshow.js
+++ javascript/slideshow.js
@@ -4,10 +4,13 @@ showSlides();
 function showSlides() {
     let i;
     let slides = document.getElementsByClassName("mySlides");
+    // calculates the ammount of slides and only shows one slide
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";  
     }
+    // increase the slide counter to move the visible slide to the next one
     slideIndex++;
+    // if it surpasses the slide ammount then it restarts its counter
     if (slideIndex > slides.length) {
         slideIndex = 1;
     }    
